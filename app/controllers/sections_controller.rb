87a13288c8dc9252feb3aaa73a20c19e28513a09
ClaudiaRuizUX <claudiaruizux@gmail.com>
2022-10-12 17:53:20 +1100

    fixes s in sections router

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,6 +1,7 @@
 class SectionsController < ApplicationController
 	def index
 		@sections = Section.all
+		@projects = Project.all
 	end  
 
 	def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   resources :skills
   resources :projects
-  resource :sections
+  resources :sections
 
   root 'projects#index'
   get '/auth/github/callback' => 'sessions#create'
   post '/' => 'sessions#destroy'
 end
-
-  
